18b21ff88dd83b95460da34226c4c54a267ca44c
GreZu28 <57643026+GreZu28@users.noreply.github.com>
2019-11-15 10:46:59 +0100

    Basics JS

---
+++ basics.js
@@ -0,0 +1,5 @@
+var miNombreCompleto = "Grecia Zuleima Alva Bermejo";
+var miEdad = 27;
+var diasSemana = ["lunes", "martes", "miércoles", "jueves", "viernes"];
+var valorGravedad = 9.8;
+var numImpares = [1, 3, 5];
